236104f87cfa7c2d03f4d34ce97e2b542ad37960
Swapnil Patil <Swapnil Patil>
2017-10-12 21:11:23 -0700

    Chrome and firefox extension added

---
+++ firefox_webextension/popup/extension.js
@@ -0,0 +1,26 @@
+
+function onExecutedPegjs(result) {
+  var querying = browser.tabs.query({ currentWindow: true, active: true });
+  var schemeinterpreter_SCRIPT = querying.executeScript(null, {
+    file: "/content_scripts/scheme_interpreter.js"
+  });
+  schemeinterpreter_SCRIPT.then(onExecutedSchemeInterpreter, onErrorSchemeInterpreter);
+}
+
+function onErrorPegjs(error) {
+  console.log(`Error adding pegjs: ${error}`);
+}
+
+
+var executingpegjs = browser.tabs.executeScript(null, {
+            file: "/content_scripts/peg-0.9.0.js"
+          });
+ 
+ executingpegjs.then(() => {
+            console.log("loaded");
+          }).error(onErrorPegjs);
+
+
+function onErrorSchemeInterpreter(error) {
+  console.log(`Error adding SchemeInterpreter: ${error}`);
+}
